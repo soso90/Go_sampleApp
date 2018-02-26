7f2b8ae4f0a56df8289384975c9adc5f55e6f40a
soso90 <hamlet55@naver.com>
2018-02-26 21:46:00 +0900

    echo webframework add

---
+++ server.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo"
+)
+
+func main() {
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+	e.GET("/show", show)
+	e.Logger.Fatal(e.Start(":1323"))
+}
+
+//e.GET("/show", show)
+func show(c echo.Context) error {
+	// Get team and member from the query string
+	team := c.QueryParam("team")
+	member := c.QueryParam("member")
+	return c.String(http.StatusOK, "team:"+team+", member:"+member)
+}
